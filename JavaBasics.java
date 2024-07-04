c1e464018f8d6cd6a601a7dbd5631a5aabfd1a08
Dibyendu Pramanik <chotuu668@gmil.com>
2024-07-04 18:08:06 +0530

    first commit

---
+++ JavaBasics.java
@@ -0,0 +1,13 @@
+
+public class JavaBasics {
+
+    public static void main(String args[]) {
+        System.out.print("**** \n");
+        System.out.print("*** \n");
+        System.out.print("** \n");
+        System.out.print("* \n");
+
+    }
+}
+//boilerplate code
+
